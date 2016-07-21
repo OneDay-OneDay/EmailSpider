05f8b4ed97359fdcecf65f0500816e80d690f8d2
JerelLin <ohlinzepeng@sina.com>
2016-07-22 00:07:11 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -6,7 +6,7 @@ module.exports = {
 		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 		"Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
 		"Accept-Encoding": "gzip, deflate, br",
-		"Cookie": "_octo=GH1.1.669343796.1468566935; logged_in=yes; _ga=GA1.2.2039296771.1468567028; _gh_sess=eyJsYXN0X3dyaXRlIjoxNDY4OTE4NzEwNDYyLCJzZXNzaW9uX2lkIjoiMzRkOTM4MmE5NGE3ODg1YzYxZDJlMGVhMjNhYTFjNDQiLCJjb250ZXh0IjoiLyIsInNweV9yZXBvIjoiSmVyZWxMaW4vYmlsaWJpbGkiLCJzcHlfcmVwb19hdCI6MTQ2ODkyMDIzNX0%3D--f19721c0f57e67ce029e1491dedd11ebb63f1ee9; tz=Asia%2FShanghai; user_session=S6sM66ogAc0OmxKTkbfqKy6gJuFEzTIRehdiRERZWGcgnc0uV43xy6XxKyvjYuI0EznRUXtPVFv-3aor; dotcom_user=JerelLin",
+		"Cookie": "",
 		"Connection": "keep-alive"
 	},
 	//获取主页链接并发连接数
