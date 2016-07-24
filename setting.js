3b19a8c9930001e9de779fa8b73062c4f08151a4
JerelLin <13531957084@163.com>
2016-07-25 01:18:10 +0800

    update

--- setting.js
+++ setting.js
@@ -1,4 +1,10 @@
 module.exports = {
+	//数据库配置
+	data_base : {
+		db : "EmailStore",
+		host : "localhost",
+		port : 27017
+	},
 	//请求头
 	header : {
 		"Host": "github.com",
@@ -6,13 +12,13 @@ module.exports = {
 		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 		"Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
 		"Accept-Encoding": "gzip, deflate, br",
-		"Cookie": "",
+		"Cookie": "_octo=GH1.1.669343796.1468566935; logged_in=yes; _ga=GA1.2.2039296771.1468567028; _gh_sess=eyJsYXN0X3dyaXRlIjoxNDY4OTE4NzEwNDYyLCJzZXNzaW9uX2lkIjoiMzRkOTM4MmE5NGE3ODg1YzYxZDJlMGVhMjNhYTFjNDQiLCJjb250ZXh0IjoiLyIsInNweV9yZXBvIjoiSmVyZWxMaW4vYmlsaWJpbGkiLCJzcHlfcmVwb19hdCI6MTQ2ODkyMDIzNX0%3D--f19721c0f57e67ce029e1491dedd11ebb63f1ee9; tz=Asia%2FShanghai; user_session=S6sM66ogAc0OmxKTkbfqKy6gJuFEzTIRehdiRERZWGcgnc0uV43xy6XxKyvjYuI0EznRUXtPVFv-3aor; dotcom_user=JerelLin",
 		"Connection": "keep-alive"
 	},
 	//获取主页链接并发连接数
-	asyncNum1 : 30,
+	asyncLink : 30,
 	//获取Email并发连接数
-	asyncNum2 : 5,
-	//目标用户名 onevcat
-	user : "onevcat"
+	asyncEmail : 5,
+	//目标用户名
+	user : "zhangmengxue"
 }
